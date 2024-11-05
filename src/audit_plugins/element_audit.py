7b54464c4534e873f4d0436d6724f50706043f29
Callum McMenamin <112744591+cmcmenamin@users.noreply.github.com>
2024-11-05 17:47:46 +1100

    Add dates to output files
    Reports generated using export_report_data.py now prepend the output filenames with the value of "output_report_name" within export_report_data_config.json. This can be used to add YYYY-MM to the start of reports.

--- src/audit_plugins/element_audit.py
+++ src/audit_plugins/element_audit.py
@@ -6,18 +6,19 @@ can be customised in the config file using a CSS selector.
 """
 
 import logging
-import selenium.common.exceptions as sel_exceptions
+from typing import Any, Union
 
-from typing import Any, Union, List
+from bs4 import BeautifulSoup
 
 from config import config
-from src.audit_manager import AuditManager
 from src.audit_plugins.default_audit import DefaultAudit
 from src.browser import Browser
-from bs4 import BeautifulSoup
+
 
 class ElementAudit:
     """element audit."""
+
+    # pylint: disable=too-many-instance-attributes
 
     audit_type = "ElementAudit"
 
@@ -39,21 +40,20 @@ class ElementAudit:
             list[Any]: rows of test data
             bool: False if test fails, else a list of results
         """
-
         # Scrape the page source of the loaded browser
         try:
             page_source = self.browser.get_page_source()
         except Exception as exc:
-            logging.error(f"Error getting page source: {exc}")
+            logging.error("Error getting page source: %s", exc)
             return False
 
         # Try to parse using BeautifulSoup
         try:
             soup = BeautifulSoup(page_source, "lxml")
         except Exception as exc:
-            logging.error(f"Error parsing page source: {exc}")
+            logging.error("Error parsing page source: %s", exc)
             return False
-        
+
         # Find all elements of the target type (css selector)
         elements = soup.select(self.target_element)
 
@@ -62,9 +62,7 @@ class ElementAudit:
         # For each element found, create a row of data
         for element in elements:
             # get element outer html
-            element_data = {
-                "element_html": element.prettify()
-            }
+            element_data = {"element_html": element.prettify()}
             found_elements.append(element_data)
 
         # Get page information from DefaultAudit
