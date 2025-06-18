04e2fd59f49d2607da8da12d7a0cad75e5fa5cb8
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-06-18 15:12:34 +1200

    feat: ensure that `<details>` elements are open before auditing

--- src/audit_manager.py
+++ src/audit_manager.py
@@ -7,6 +7,7 @@ import urllib.parse
 from typing import Any, Type
 
 import selenium
+from selenium.webdriver.common.by import By
 
 import src.output
 from config import config
@@ -138,6 +139,27 @@ class AuditManager:
 
         return status
 
+    def ensure_details_are_open(self) -> None:
+        """Ensure that <details> elements are open so their contents are audited."""
+        details = self.browser.driver.find_elements(By.TAG_NAME, "details")
+
+        if len(details) == 0:
+            return
+
+        plural = ""
+        if len(details) != 1:
+            plural = "s"
+        logging.info("opening %i <details> element%s", len(details), plural)
+
+        # Open all <details> elements
+        for detail in details:
+            if not detail.get_attribute("open"):
+                try:
+                    self.browser.driver.execute_script("arguments[0].setAttribute('open', '')", detail)
+                except Exception:  # pylint: disable=broad-except
+                    logging.warning("Could not open <details> element titled '%s'", detail.text)
+                    continue
+
     def run_audits(self) -> bool:
         """Iterate through registered audits and runs them.
 
@@ -199,6 +221,8 @@ class AuditManager:
                         audit["kwargs"]["url"],
                     )
                     continue
+
+                self.ensure_details_are_open()
 
                 # Inject the audit ID
                 audit["kwargs"]["audit_id"] = audit_id
