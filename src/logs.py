9f12b6b58f8d8a2bd549751741a850763b299747
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-07-14 14:59:59 +1200

    feat: support capturing logs for base urls in dedicated files

--- src/logs.py
+++ src/logs.py
@@ -2,9 +2,10 @@
 
 import logging
 import os
+import re
 import threading
 from contextlib import contextmanager
-from typing import Any, Generator
+from typing import Any, Callable, Generator
 
 
 def __register_thread_based_file_handler(directory: str, prefix: str) -> logging.Handler:
@@ -13,6 +14,17 @@ def __register_thread_based_file_handler(directory: str, prefix: str) -> logging
 
     log_handler = create_file_log_handler(log_file)
     log_handler.addFilter(lambda record: record.threadName == thread_name)
+
+    logging.getLogger().addHandler(log_handler)
+
+    return log_handler
+
+
+def __create_and_register_selective_file_handler(
+    log_file: str, selector: Callable[[logging.LogRecord], bool | logging.LogRecord]
+) -> logging.Handler:
+    log_handler = create_file_log_handler(log_file)
+    log_handler.addFilter(selector)
 
     logging.getLogger().addHandler(log_handler)
 
@@ -40,6 +52,45 @@ def group_by_thread(directory: str, prefix: str) -> Generator[None, Any, None]:
     """Group logs made by the current thread into a secondary file."""
     os.makedirs(directory, exist_ok=True)
     log_handler = __register_thread_based_file_handler(directory, prefix)
+    try:
+        yield
+    finally:
+        logging.getLogger().removeHandler(log_handler)
+        log_handler.close()
+
+
+# todo: who knows if this is thread safe enough...
+matchups = {}
+
+
+def sanitise_string(string: str) -> str:
+    """Sanitise a string for use in a folder/filename.
+
+    Args:
+        string (str): the string to sanitise
+
+    Returns:
+        str: a sanitised string
+    """
+    temp_str = string.strip()
+    temp_str = re.sub(r"[^a-zA-Z0-9_\-.]", "_", temp_str)
+    temp_str = re.sub(r"_+", "_", temp_str)
+    temp_str = temp_str[:50]
+    return temp_str
+
+
+@contextmanager
+def group_by_base_url(directory: str, base_url: str) -> Generator[None, Any, None]:
+    """Group logs for the given base_url into a dedicated secondary file."""
+    os.makedirs(directory, exist_ok=True)
+    safe_base_url = sanitise_string(base_url)
+
+    matchups[threading.current_thread().ident] = base_url
+    log_handler = __create_and_register_selective_file_handler(
+        f"{directory}/{safe_base_url}.log",
+        lambda record: matchups.get(record.thread, "") == base_url,
+    )
+
     try:
         yield
     finally:
