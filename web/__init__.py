544b51f22583567386ea7e60674b83555ee35ac3
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-29 10:56:19 +1200

    feat: add skeleton index, configs, and urls pages

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return '<p>Hello, World!</p>'
+def root() -> str:
+  return render_template('index.html')
+
+
+@app.route('/configs')
+def view_configs() -> str:
+  return render_template('configs.html')
+
+
+@app.route('/urls')
+def view_urls() -> str:
+  return render_template('urls.html')
