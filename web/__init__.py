3c38d91cdba646ebaf630b29f9cb382a590bf6d1
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-29 14:30:22 +1200

    feat: parse urls from disk

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,27 @@
+import csv
 import os
+from typing import TypedDict, cast
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+# todo: this has been copied from config.py to avoid loading the config
+class Endpoint(TypedDict):
+  """Holds data for a url that should be audited and potentially crawled."""
+
+  organisation: str
+  url: str
+  sector: str
+
+
+class EndpointsFile(TypedDict):
+  """Holds a list of endpoints that should be audited and potentially crawled."""
+
+  path: str
+  endpoints: list[Endpoint]
+  invalid_reason: str
 
 
 @app.route('/')
@@ -20,8 +39,32 @@ def view_configs() -> str:
 
 @app.route('/urls')
 def view_urls() -> str:
-  return render_template(
-    'urls.html',
-    # todo: we should be taking the "base_urls_visit_path" of the "current config" into account
-    files=[c for c in os.listdir('./base_urls/visit') if c.endswith('.csv')],
-  )
+  files: list[EndpointsFile] = []
+
+  folder_path = './base_urls/visit/'
+
+  # todo: we should be taking the "base_urls_visit_path" of the "current config" into account
+  for filename in os.listdir(folder_path):
+    if filename.endswith('.csv'):
+      with open(
+        os.path.join(folder_path, filename),
+        encoding='utf-8-sig',
+        newline='',
+      ) as file:
+        endpoint_file = EndpointsFile(
+          path=os.path.join(folder_path, filename),
+          endpoints=[],
+          invalid_reason='',
+        )
+        files.append(endpoint_file)
+        reader = csv.reader(file)
+        header = next(reader)
+        for row in reader:
+          if len(row) != 3:
+            endpoint_file['invalid_reason'] = 'CSV files must have 3 columns'
+            break
+
+          subject = cast(Endpoint, dict(zip(header, row)))
+          endpoint_file['endpoints'].append(subject)
+
+  return render_template('urls.html', files=files)
