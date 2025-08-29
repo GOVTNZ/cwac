c38ff988f2445d2e1509a491a1b8c63b8228b080
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-29 12:54:01 +1200

    feat: add basic reading of config and url directories

--- web/__init__.py
+++ web/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,9 +12,16 @@ def root() -> str:
 
 @app.route('/configs')
 def view_configs() -> str:
-  return render_template('configs.html')
+  return render_template(
+    'configs.html',
+    files=[c for c in os.listdir('./config') if c.endswith('.json')],
+  )
 
 
 @app.route('/urls')
 def view_urls() -> str:
-  return render_template('urls.html')
+  return render_template(
+    'urls.html',
+    # todo: we should be taking the "base_urls_visit_path" of the "current config" into account
+    files=[c for c in os.listdir('./base_urls/visit') if c.endswith('.csv')],
+  )
