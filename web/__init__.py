d7217c1fa43dd75bc954d8aca050e719412dd016
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-29 15:49:56 +1200

    fix: ensure that missing and empty directories are handled

--- web/__init__.py
+++ web/__init__.py
@@ -1,3 +1,4 @@
+import contextlib
 import csv
 import os
 from typing import TypedDict, cast
@@ -31,40 +32,43 @@ def root() -> str:
 
 @app.route('/configs')
 def view_configs() -> str:
-  return render_template(
-    'configs.html',
-    files=[c for c in os.listdir('./config') if c.endswith('.json')],
-  )
+  try:
+    files = [c for c in os.listdir('./config') if c.endswith('.json')]
+  except FileNotFoundError:
+    files = []
+
+  return render_template('configs.html', files=files)
 
 
 @app.route('/urls')
 def view_urls() -> str:
   files: list[EndpointsFile] = []
 
+  # todo: we should be taking the "base_urls_visit_path" of the "current config" into account
   folder_path = './base_urls/visit/'
 
-  # todo: we should be taking the "base_urls_visit_path" of the "current config" into account
-  for filename in os.listdir(folder_path):
-    if filename.endswith('.csv'):
-      with open(
-        os.path.join(folder_path, filename),
-        encoding='utf-8-sig',
-        newline='',
-      ) as file:
-        endpoint_file = EndpointsFile(
-          path=os.path.join(folder_path, filename),
-          endpoints=[],
-          invalid_reason='',
-        )
-        files.append(endpoint_file)
-        reader = csv.reader(file)
-        header = next(reader)
-        for row in reader:
-          if len(row) != 3:
-            endpoint_file['invalid_reason'] = 'CSV files must have 3 columns'
-            break
+  with contextlib.suppress(FileNotFoundError):
+    for filename in os.listdir(folder_path):
+      if filename.endswith('.csv'):
+        with open(
+          os.path.join(folder_path, filename),
+          encoding='utf-8-sig',
+          newline='',
+        ) as file:
+          endpoint_file = EndpointsFile(
+            path=os.path.join(folder_path, filename),
+            endpoints=[],
+            invalid_reason='',
+          )
+          files.append(endpoint_file)
+          reader = csv.reader(file)
+          header = next(reader)
+          for row in reader:
+            if len(row) != 3:
+              endpoint_file['invalid_reason'] = 'CSV files must have 3 columns'
+              break
 
-          subject = cast(Endpoint, dict(zip(header, row)))
-          endpoint_file['endpoints'].append(subject)
+            subject = cast(Endpoint, dict(zip(header, row)))
+            endpoint_file['endpoints'].append(subject)
 
   return render_template('urls.html', files=files)
