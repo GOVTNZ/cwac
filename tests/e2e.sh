30f2d32b36083a3ece021fe4dae31c07e8f40c98
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-07-15 09:18:35 +1200

    test: set up an end-to-end run

---
+++ tests/e2e.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+# create a url file
+cat <<URLS > base_urls/visit/e2e.csv
+organisation,url,sector
+e2e,example.com,e2e
+URLS
+
+# create a config file
+cat config/config_default.json | jq '
+  .max_links_per_domain = 3 |
+  .filter_to_organisations = ["e2e"] |
+  .filter_to_urls = ["example.com"]
+' > config/config_e2e.json
+
+docker build --iidfile /tmp/cwac_image_id .
+
+docker run --rm \
+  --mount "type=bind,src=./config,dst=/cwac/config" \
+  --mount "type=bind,src=./base_urls,dst=/cwac/base_urls" \
+  --mount "type=bind,src=./results,dst=/cwac/results" \
+  -e CHROME_EXTRA_ARGS='--user-data-dir=/tmp,--no-sandbox,--disable-dev-shm-usage' \
+  $(cat /tmp/cwac_image_id) .venv/bin/python -u cwac.py config_e2e.json
