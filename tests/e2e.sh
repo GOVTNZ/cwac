db8c54320bb2e1e872faa5b50df84a76c876f6ac
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-07-20 09:34:15 +1200

    test: ensure all audits work in docker image (#285)

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -14,7 +14,7 @@ cat config/config_default.json | jq '
   .max_links_per_domain = 3 |
   .filter_to_organisations = ["e2e"] |
   .filter_to_urls = ["example.com"] |
-  .audit_plugins.screenshot_audit.enabled = true
+  .audit_plugins |= map_values(.enabled = true)
 ' > config/config_e2e.json
 
 # make sure the "results" directory exists
