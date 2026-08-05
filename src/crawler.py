3db413ff1e36338524c9e49ac806620120fb9afc
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-08-05 14:34:25 +1200

    fix: make sure the `base_url` is audited before introducing urls from the sitemap

--- src/crawler.py
+++ src/crawler.py
@@ -523,13 +523,7 @@ class Crawler:
       logger.error('base_url was filtered out! %s', base_url)
       return
 
-    if self.config.crawl_sitemaps:
-      urls_from_sitemap = self.__crawl_sitemap(base_url)
-
-      logger.info('Found %i url%s from sitemaps', len(urls_from_sitemap), '' if len(urls_from_sitemap) == 1 else 's')
-
-      for parent_and_url in urls_from_sitemap:
-        queue.push(parent_and_url)
+    has_crawled_sitemap = not self.config.crawl_sitemaps
 
     while queue:
       parent_url, url = queue.pop()
@@ -611,6 +605,17 @@ class Crawler:
       # don't bother getting links if we are only scanning one link per base url
       if self.config.max_links_per_domain == 1:
         break
+
+      # we delay crawling the sitemap for urls until after the base_url has been
+      # audited to ensure that has happened since the queue provides urls at random
+      if len(queue) == 0 and not has_crawled_sitemap:
+        urls_from_sitemap = self.__crawl_sitemap(base_url)
+
+        logger.info('Found %i url%s from sitemaps', len(urls_from_sitemap), '' if len(urls_from_sitemap) == 1 else 's')
+
+        for parent_and_url in urls_from_sitemap:
+          queue.push(parent_and_url)
+        has_crawled_sitemap = True
 
       links = self.get_links(base_url, url)
 
